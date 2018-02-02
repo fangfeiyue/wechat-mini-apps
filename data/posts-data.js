e2246a6f6a91a447088f6c336a286023aed7a5c9
FangFeiyue <18766131540@163.com>
2018-02-02 18:21:22 +0800

    使用data-传参

--- data/posts-data.js
+++ data/posts-data.js
@@ -6,7 +6,8 @@ var postData = [{
         content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
         reading: "112",
         collection: "96",
-        image_condition: true
+        image_condition: true,
+        postId: 0
     },
     {
         title: "比利·林恩的中场故事",
@@ -20,7 +21,8 @@ var postData = [{
         author: "迷的城",
         date: "Nov 20 2016",
         avatar: "/images/avatar/1.png",
-        image_condition: true
+        image_condition: true,
+        postId: 1
     },
     {
         title: "当我们在谈论经济学时，我们在谈论什么?",
@@ -34,7 +36,8 @@ var postData = [{
         date: "Nov 12 2016",
         dateTime: "三天前",
         avatar: "/images/avatar/3.png",
-        image_condition: true
+        image_condition: true,
+        postId: 2
     }, {
         title: "微信·小程序开发工具安装指南",
         content: "这两天闲来无事，也安装了 “微信折叠”的开发工具来玩一下。以下是一些小道消息及使用体验，过两天我会写一篇文章以开发者的角度来详细评价微信小程序",
@@ -47,7 +50,8 @@ var postData = [{
         author: "猫是猫的猫",
         date: "Nov 20 2016",
         avatar: "/images/avatar/5.png",
-        image_condition: true
+        image_condition: true,
+        postId: 3
     }, {
         title: "从视觉到触觉 这款VR手套能给你真实触感",
         content: "8月29日消息，据国外媒体VentureBeat报道，一家名为Dexta Robotics的公司最近发布了一款有望变革虚拟现实手部追踪与交互方式的新产品",
@@ -60,7 +64,8 @@ var postData = [{
         author: "深白色",
         date: "Nov 20 2016",
         avatar: "../../../images/avatar/3.png",
-        image_condition: true
+        image_condition: true,
+        postId: 4
     }, {
         title: "爱奇艺创维开展战略合作，合力布局开放娱乐生态",
         content: "爱奇艺和创维分别作为国内领先的在线视频品牌",
@@ -73,7 +78,8 @@ var postData = [{
         author: "深白色",
         date: "Nov 20 2016",
         avatar: "../../../images/avatar/5.png",
-        image_condition: true
+        image_condition: true,
+        postId: 5
     }
 ];
 
--- pages/posts/post.js
+++ pages/posts/post.js
@@ -4,8 +4,12 @@ Page({
     data:{
         message: 'hello world'
     },
-    onLoad: function(options){
+    onLoad(options){
         // 页面初始化，options为页面跳转所带来的参数
         this.setData({ postData: postData.postData });
-    }
+    },
+    onPostTap(event){
+        let postId = event.currentTarget.dataset.postid;
+        console.log(postId);
+    },
   })
