2b948a363b927deb6f7baa38fea02cb11e1d0d55
FangFeiyue <18766131540@163.com>
2018-02-05 14:23:10 +0800

    文章收藏功能

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,20 +1,42 @@
 let postData = require('../../../data//posts-data.js');
 
 Page({
+    data:{
+        collected:''
+    },
     onLoad(options){
         let postId = options.id,
-            postDataArr = postData.postData[postId];
+            postDataArr = postData.postData[postId],
+            postsCollected = wx.getStorageInfoSync('posts_collected');
+
+        this.data.currentPostId = postId;
 
         this.setData({
             ...postDataArr
         });
 
-        wx.setStorageSync('name', 'fangfeiyue');
+        if (postsCollected){
+            this.setData({
+                collected: postsCollected[postId]
+            });
+        }else{
+            wx.setStorageSync('posts_collected', {
+                postId: false
+            });
+        }
     },
     onCollectionTap(event){
-        let name = wx.getStorageSync('name');
+        const { currentPostId } = this.data,
+              postsCollected    = wx.getStorageSync('posts_collect'),
+              postCollected     = postsCollected[currentPostId];
 
-        console.log(name);
+        wx.setStorageSync('posts_collect', {
+            [currentPostId]: !postCollected
+        });
+
+        this.setData({
+            collected: postCollected
+        });
     },
     onShareTap(){
         wx.removeStorageSync('name');
