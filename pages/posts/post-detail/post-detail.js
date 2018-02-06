753effb811863f15665ec923734e3f0dc1c5295e
FangFeiyue <18766131540@163.com>
2018-02-06 13:37:47 +0800

    showActionSheet

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -3,6 +3,22 @@ let postData = require('../../../data//posts-data.js');
 Page({
     data:{
     },
+    onShareAppMessage(res){
+        if (res.from === 'button') {
+          // 来自页面内转发按钮
+          console.log(res.target)
+        }
+        return {
+          title: '自定义转发标题',
+          path: '/page/user?id=123',
+          success: function(res) {
+            // 转发成功
+          },
+          fail: function(res) {
+            // 转发失败
+          }
+        }
+    },
     onLoad(options){
         let postId = options.id,
             postDataArr = postData.postData[postId],
@@ -41,13 +57,39 @@ Page({
         });
 
         wx.showToast({
-            title: !postCollected ? '收藏成功':'取消成功',
+            title: !postCollected ? '收藏成功' : '取消成功',
             icon: 'success',
             duration: 2000
-        })
+        });
+
+        // wx.showModal({
+        //     title: '收藏',
+        //     content: '是否收藏文章',
+        //      success: function(res) {
+        //       if (res.confirm) {
+        //       } else if (res.cancel) {
+        //       }
+        //     }
+        // });
     },
     onShareTap(){
-        wx.removeStorageSync('name');
-        wx.clearStorageSync();
+        const itemList = ['分享到微信好友', '分享到朋友圈', '分享到QQ', '分享到微博'];
+
+        wx.showActionSheet({
+            itemList: itemList,
+            itemColor: '#405f80',
+            success: function(res) {
+                wx.showModal({
+                    title: '用户' + itemList[res.tapIndex],
+                    content: '哎，现在无法实现分享功能，不知道什么时候支持'
+                });
+            },
+            fail: function(res) {
+              // 用户点击了取消会走到fail函数中  
+              console.log(res.errMsg);
+            }
+        });
+
+
     }
 });
