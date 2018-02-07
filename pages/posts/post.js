e04864b25f1d51ca88da4934693a7b29b4e05b80
FangFeiyue <18766131540@163.com>
2018-02-07 15:39:24 +0800

    点击文章轮播图上的图片跳转到对应的详情

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -14,4 +14,12 @@ Page({
         })
         console.log(postId);
     },
+    onSwiperTap(event){
+        // target指的是当前点击的组件；currentTarget指的是事件捕获组件
+        let postId = event.target.dataset.postid;
+        console.log(event);
+        wx.navigateTo({
+            url: `./post-detail/post-detail?id=${postId}`
+        })
+    }
   })
