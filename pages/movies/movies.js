6565ce3594332a472d94930c25c7d35397fb2d6d
FangFeiyue <18766131540@163.com>
2018-02-09 19:34:03 +0800

    请求网路数据

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,3 +1,19 @@
 Page({
-    
+    onLoad(){
+        wx.request({
+            url: 'http://t.yushu.im/v2/movie/top250',
+            data: {},
+            method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+            // header: {}, // 设置请求的 header
+            success: function(res){
+                console.log(res);
+            },
+            fail: function() {
+                // fail
+            },
+            complete: function() {
+                // complete
+            }
+        })
+    }
 });
