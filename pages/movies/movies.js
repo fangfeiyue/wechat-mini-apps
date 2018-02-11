9366304afd71bde992ba64fd7195bbd400cceca1
FangFeiyue <18766131540@163.com>
2018-02-11 10:11:01 +0800

    调用正在热映、即将上映、top250电影相关接口

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,10 +1,22 @@
 let app = getApp();
+let doubanBase = app.globalData.doubanBase;
 
 Page({
     onLoad(){
-        console.log('app==>',app);
+        let inTheatersUrl = `${doubanBase}/v2/movie/in_theaters`;
+        let comingSoonUrl = `${doubanBase}/v2/movie/coming_soon`;
+        let top250Url = `${doubanBase}/v2/movie/top250`;
+
+        // 正在热映
+        this.getMovieData(inTheatersUrl);
+        // 即将上映
+        this.getMovieData(comingSoonUrl);
+        // top250
+        this.getMovieData(top250Url);
+    },
+    getMovieData(url){
         wx.request({
-            url: `${app.globalData.doubanBase}/v2/movie/top250`,
+            url: `${doubanBase}/v2/movie/top250`,
             data: {},
             method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
             // header: {}, // 设置请求的 header
