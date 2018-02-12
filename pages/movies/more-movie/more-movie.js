852f63e816e4e9db4082026e8df1ffc83587b2c2
FangFeiyue <18766131540@163.com>
2018-02-12 10:45:11 +0800

    动态设置导航栏标题

--- pages/movies/more-movie/more-movie.js
+++ pages/movies/more-movie/more-movie.js
@@ -1,7 +1,20 @@
 Page({
-    data: {},
+    data: {
+        navigateTitle: ''
+    },
     onLoad(options){
         let category = options.category;
         console.log(category);
+        this.setData({
+            navigateTitle: category
+        });
+    },
+    // 当页面已经准备完毕才执行
+    onReady(event){
+
+        // 动态设置导航栏标题
+        wx.setNavigationBarTitle({
+            title: this.data.navigateTitle
+        });
     }
 });
