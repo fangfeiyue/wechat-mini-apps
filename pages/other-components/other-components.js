589ed8ad0815f09c3c95ed0a176ac655ef07ffbd
FangFeiyue <18766131540@163.com>
2018-02-28 16:30:12 +0800

    表单控件

--- pages/other-components/other-components.js
+++ pages/other-components/other-components.js
@@ -1,24 +1,33 @@
 Page({
-    onLoad(options){
+    onLoad(options) {
 
     },
-    switchChange(event){
+    switchChange(event) {
         console.log('切换了', event.detail.value);
     },
     // 完成一次拖动后触发的事件
-    sliderChange(){
+    sliderChange() {
         console.log('完成一次拖动后触发的事件');
     },
     // 拖动过程中触发的事件
-    sliderChanging(){
+    sliderChanging() {
         console.log('拖动过程中触发的事件');
     },
     // <radio-group/> 中的选中项发生变化时触发 change 事件
-    radioChange(event){
+    radioChange(event) {
         console.log('radio的value值====》', event.detail.value);
     },
     // 表单提交
-    submitForm(event){
-        console.log(event);
+    submitForm(event) {
+        console.log('form发生了submit事件，携带数据为：', event.detail.value);
+    },
+    resetForm(event) {
+        console.log('form发生了reset事件')
+    },
+    formSubmit: function (e) {
+        console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    },
+    formReset: function () {
+        console.log('form发生了reset事件')
     }
 });
